e00e3eb098e1d22f9ea28e224173bf1bcfd44fd8
bismarckAlves <bismarckalvess@gmail.com>
2024-09-16 22:12:57 -0300

    versao aula

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { StudyRouteParamsComponent } from './pages/study-route-params/study-route-params.component';
-import { StudyQueryParamsComponent } from './pages/study-query-params/study-query-params.component';
-import { StudyNestedRoutesComponent } from './pages/study-nested-routes/study-nested-routes.component';
-import { PeopleComponent } from './pages/study-nested-routes/components/people/people.component';
-import { CitiesComponent } from './pages/study-nested-routes/components/cities/cities.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudyRouteParamsComponent,
-    StudyQueryParamsComponent,
-    StudyNestedRoutesComponent,
-    PeopleComponent,
-    CitiesComponent,
   ],
   imports: [
     BrowserModule,
